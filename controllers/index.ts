cd966595fbe4f8f5f07b9b453a31e721abb3c095
Eliecer Molina <eliecer.molina@uruit.com>
2023-04-04 17:54:19 -0500

    CHECKPOINT: adding example of using controller decorators

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,8 +1,25 @@
 import { AwsProxyRouter } from 'simple-lambda-api-router/dist/classes';
 import HealthController from './health';
+import userController from './user';
+import clientController from './client';
 
 const router = new AwsProxyRouter();
+
+// using createControlle approach
 router.use('/health/:parameter/status/:optional?', HealthController);
+
+// using decorators approach
+
+router.use(userController.getUser);
+router.use(userController.getUsers);
+router.use(userController.createUser);
+router.use(userController.deleteUser);
+
+router.use(clientController.getClient);
+router.use(clientController.getClients);
+router.use(clientController.createClient);
+router.use(clientController.deleteClient);
+
 const handler = router.expose();
 
 export default handler;
